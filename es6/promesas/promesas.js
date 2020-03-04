197aa031367384f724a6f8396a6ff0173187963e
Roman <romazukov1993@gmail.com>
2020-03-04 23:10:04 +0100

    Promesas

---
+++ es6/promesas/promesas.js
@@ -0,0 +1,17 @@
+let getCoins = new Promise((resolve, reject) => {
+    // Invocamos resolve() cuando lo que estabamos haciendo asincronamente ha terminado con exito, en caso contrario invocamos reject()
+    // En este ejemplo, utilizamos setTimeout() para simular una operacion asincrona
+    setTimeout( function() {
+        resolve("10 coins!");
+    }, 3000);
+});
+
+const coins = () => {
+    console.log('Get coins...');
+    getCoins.then((success) => {
+        console.log(`He obtenido ${success}`);
+    });
+    console.log('Continue executing...');
+};
+
+coins();
