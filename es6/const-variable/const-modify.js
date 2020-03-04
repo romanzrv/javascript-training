0469c5203a60f7f78a5d938b735efcf13b5b8d93
Roman <romazukov1993@gmail.com>
2020-03-04 20:49:21 +0100

    const

---
+++ es6/const-variable/const-modify.js
@@ -0,0 +1,11 @@
+const myConst = {coin: 'coin'};
+myConst.virus = 'coinronavirus';
+
+const myArray = ['Coin', 'Zancos'];
+myArray.push('codurans');
+
+console.log(myConst);
+console.log(myArray);
+
+// myConst = {}; // Error: Can't reassign
+// myArray = []; // Error: Can't reassign
---
+++ es6/const-variable/const-variable.js
@@ -0,0 +1,6 @@
+const constant = 'constant-value';
+// const constant; // Error: Must initialize with a value
+// constant = 'other-value'; // Error: Can not modify the value
+
+console.log(constant);
+
